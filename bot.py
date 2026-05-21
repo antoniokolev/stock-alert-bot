980235f9c8008953c161618ca71b061f6b3c8538
antoniokolev <antoniokolev@gmail.com>
2026-05-21 11:43:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ STOCKS = [
     {"symbol": "ONON",  "name": "On Holding AG"},
     {"symbol": "^GSPC", "name": "S&P 500 Index"},
     {"symbol": "PLTR",  "name": "Palantir Tech"},
-    {"symbol": "SHELL.AS",  "name": "Shell plc (Amsterdam)"},
+    {"symbol": "SHELL.AS", "name": "Shell plc (Amsterdam)"},
     {"symbol": "NVDA",  "name": "Nvidia"},
     {"symbol": "AMD",   "name": "AMD"},
     {"symbol": "SLV",   "name": "Silver"},
